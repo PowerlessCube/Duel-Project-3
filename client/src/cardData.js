a93fab061e633ab2bd7887abf66e3ae4552070a1
Alistair Mackay <alistair.mackay.work@gmail.com>
2016-07-08 11:40:18 +0100

    Passing through card data to initial state of GameBox

---
+++ client/src/cardData.js
@@ -0,0 +1,50 @@
+module.exports = [
+	//Player 1 - Cards
+	[
+		{
+			player: 1,
+			powerLevel: 0,
+			cardImage: "Image link",
+			cardTitle: "Killing Blow",
+			CardAbilityText: "Bury a face up card. Cannot play after trump."
+		},
+		{
+			player: 1,
+			powerLevel: 1,
+			cardImage: "Image link",
+			cardTitle: "Hidden Strength",
+			CardAbilityText: "Draw a random buried card."
+		},
+		{
+			player: 1,
+			powerLevel: 2,
+			cardImage: "Image link",
+			cardTitle: "Precision Strike",
+			CardAbilityText: "Put a face-up card into your hand."
+		}
+	],
+	//Player 2 - Cards
+	[
+		{
+			player: 2,
+			powerLevel: 0,
+			cardImage: "Image link",
+			cardTitle: "Killing Blow",
+			CardAbilityText: "Bury a face up card. Cannot play after trump"
+		},
+		{
+			player: 2,
+			powerLevel: 1,
+			cardImage: "Image link",
+			cardTitle: "Hidden Strength",
+			CardAbilityText: "Draw a random buried card."
+		},
+		{
+			player: 2,
+			powerLevel: 2,
+			cardImage: "Image link",
+			cardTitle: "Precision Strike",
+			CardAbilityText: "Put a face-up card into your hand."
+		}
+	]
+];
--- client/src/components/GameBox.jsx
+++ client/src/components/GameBox.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var PlayerBox = require('./PlayerBox.jsx');
 var SharedGameStateBox = require('./SharedGameStateBox.jsx');
+var cardData = require('../cardData.js');
 
 var GameBox = React.createClass({
 	// TODO: The following details will be in the state:
@@ -9,14 +10,22 @@ var GameBox = React.createClass({
 	// GameStateBox
 	// - Tower of Power - The pile of face-up played cards
 	// - Bury Pile - The pile of face-down cards
+	getInitialState: function() {
+		return {
+			PlayerCards: cardData
+		}
+	},
 
 	render: function() {
 		return (
 			<div className="game-box">
 				Game Box Loaded
 				<SharedGameStateBox />
-				<PlayerBox />
-				<PlayerBox />
+				<PlayerBox
+					cardData={this.state.playerCards}
+				/>
+				<PlayerBox
+				/>
 			</div>
 		)
 	}
