8bcd0c72cb4db2e98026ef329ceca533a584c378
Alistair Mackay <alistair.mackay.work@gmail.com>
2016-07-09 23:17:37 +0100

    can call functions from my models folders into my react views

--- client/src/components/GameBox.jsx
+++ client/src/components/GameBox.jsx
@@ -2,8 +2,9 @@ var React = require('react');
 var PlayerBox = require('./PlayerBox.jsx');
 var SharedGameStateBox = require('./SharedGameStateBox.jsx');
 var CardData = require('../cardData.js');
-var GameState = require('../models/gameState.js');
+var GameState = require('./models/gameState.js');
 
+var GameState = new GameState();
 var GameBox = React.createClass({
 	// TODO: The following details will be in the state:
 	// Player box
@@ -13,9 +14,16 @@ var GameBox = React.createClass({
 	// - Bury Pile - The pile of face-down cards
 	getInitialState: function() {
 		return {
-			// players: GameState.addPlayer("Player 1"),
-			playerCards: CardData
+			playerCards: CardData,
+			players: GameState.addPlayers(CardData),
+			currentPlayer: GameState.nextPlayer()
 		}
+	},
+
+	changePlayer: function(e) {
+		let nextPlayer = GameState.nextPlayer();
+		this.setState({currentPlayer: nextPlayer})
+		console.log("Who is the current player? ", this.state.currentPlayer );
 	},
 
 	render: function() {
@@ -29,6 +37,7 @@ var GameBox = React.createClass({
 				<PlayerBox
 					CardData={this.state.playerCards[1]}
 				/>
+			<input type="submit" value="Change player" onClick={this.changePlayer}/>
 			</div>
 		)
 	}
