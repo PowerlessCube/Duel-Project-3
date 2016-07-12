add98be108549b8a5c14079d890f2149d95a38f5
Alistair Mackay <alistair.mackay.work@gmail.com>
2016-07-12 16:33:47 +0100

    added additional card to the cardData

--- client/src/components/models/cardData.js
+++ client/src/components/models/cardData.js
@@ -23,6 +23,48 @@ module.exports = [
 					cardBackImage: "image/card-back.png",
 					cardTitle: "Precision Strike",
 					CardAbilityText: "Put a face-up card into your hand."
+				},
+				{
+					powerLevel: 3,
+					cardImage: "image/redcards/red-3.png",
+					cardBackImage: "image/card-back.png",
+					cardTitle: "Feint",
+					CardAbilityText: "Play a random card from your target's hand. Ignore the ability."
+				},
+				{
+					powerLevel: 4,
+					cardImage: "image/redcards/red-4.png",
+					cardBackImage: "image/card-back.png",
+					cardTitle: "Copycat",
+					CardAbilityText: "Copy the ability of any face up card (2 players)."
+				},
+				{
+					powerLevel: 5,
+					cardImage: "image/redcards/red-5.png",
+					cardBackImage: "image/card-back.png",
+					cardTitle: "Change Stance",
+					CardAbilityText: "The next card played must be greate than 5."
+				},
+				{
+					powerLevel: 6,
+					cardImage: "image/redcards/red-6.png",
+					cardBackImage: "image/card-back.png",
+					cardTitle: "Disarm",
+					CardAbilityText: "Target buries a card at random."
+				},
+				{
+					powerLevel: 7,
+					cardImage: "image/redcards/red-7.png",
+					cardBackImage: "image/card-back.png",
+					cardTitle: "Back Stab",
+					CardAbilityText: "Target gives you a card of their choice."
+				},
+				{
+					powerLevel: 8,
+					cardImage: "image/redcards/trump-red.png",
+					cardBackImage: "image/card-back.png",
+					cardTitle: "Dodge",
+					CardAbilityText: "Trumps last card played then counts as an 8. 0 (Killing Blow) cannot be played next."
 				}
 			]
 		},
@@ -51,6 +93,48 @@ module.exports = [
 					cardBackImage: "image/card-back.png",
 					cardTitle: "Precision Strike",
 					CardAbilityText: "Put a face-up card into your hand."
+				},
+				{
+					powerLevel: 3,
+					cardImage: "image/purplecards/purple-3.png",
+					cardBackImage: "image/card-back.png",
+					cardTitle: "Feint",
+					CardAbilityText: "Play a random card from your target's hand. Ignore the ability."
+				},
+				{
+					powerLevel: 4,
+					cardImage: "image/purplecards/purple-4.png",
+					cardBackImage: "image/card-back.png",
+					cardTitle: "Copycat",
+					CardAbilityText: "Copy the ability of any face up card (2 players)."
+				},
+				{
+					powerLevel: 5,
+					cardImage: "image/purplecards/purple-5.png",
+					cardBackImage: "image/card-back.png",
+					cardTitle: "Change Stance",
+					CardAbilityText: "The next card played must be greate than 5."
+				},
+				{
+					powerLevel: 6,
+					cardImage: "image/purplecards/purple-6.png",
+					cardBackImage: "image/card-back.png",
+					cardTitle: "Disarm",
+					CardAbilityText: "Target buries a card at random."
+				},
+				{
+					powerLevel: 7,
+					cardImage: "image/purplecards/purple-7.png",
+					cardBackImage: "image/card-back.png",
+					cardTitle: "Back Stab",
+					CardAbilityText: "Target gives you a card of their choice."
+				},
+				{
+					powerLevel: 8,
+					cardImage: "image/purplecards/trump-purple.png",
+					cardBackImage: "image/card-back.png",
+					cardTitle: "Dodge",
+					CardAbilityText: "Trumps last card played then counts as an 8. 0 (Killing Blow) cannot be played next."
 				}
 			]
 		}
--- client/src/components/models/cardMovement.js
+++ client/src/components/models/cardMovement.js
@@ -1,31 +1,12 @@
 //MOVING CARDS FROM ARRAYS
 module.exports = {
 
-	// findCard: function(deck, findCard) {
-	// 	for (var card of deck) {
-	// 		if (card === findCard) {
-	// 			return card;
-	// 		}
-	// 	}
-	// },
-
 	removeCard: function(deck, cardToRemove) {
 		return deck.filter(function(card){
 			return cardToRemove.powerLevel != card.powerLevel;
 		});
 	},
 
-	// addCard: function(deck, card) {
-	// 	deck.unshift(card);
-	// },
-	//
-	// moveCard: function(fromDeck, card, targetDeck) {
-	// 	var targetCard = this.findCard(fromDeck, card);
-	// 	this.removeCard(fromDeck, targetCard);
-	// 	this.addCard(targetDeck, targetCard);
-	// },
-
-	//Randomised Selection Function
 	randomCard: function(deck) {
 		return deck[Math.floor(Math.random() * deck.length)];
 	}
