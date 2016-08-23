114a7f9bd20de94139778dbe07e955773d81793a
Alistair Mackay <alistair.mackay.work@gmail.com>
2016-08-23 07:50:40 +0100

    changed to lh:3001 and changed the titel

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var path = require('path')
+var path = require('path');
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
@@ -8,8 +8,7 @@ app.get('/', function (req, res) {
 
 app.use(express.static('client/build'));
 
-
-var server = app.listen(3000, function () {
+var server = app.listen(3001, function () {
   var host = server.address().address;
   var port = server.address().port;
 
